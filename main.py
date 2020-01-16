042cb1f4266bb39c4b076f158fb97f11cf6ae45f
Claudio <claudiojlf@gmail.com>
2020-01-16 14:43:15 -0300

    architecturing the main file

---
+++ main.py
@@ -0,0 +1,14 @@
+def draw_Grid(w, rows, surface):
+    pass
+
+def redraw_Window(surface):
+    pass
+
+def random_Snack(rows, items):
+    pass
+
+def message_Box():
+    pass
+
+def main():
+    pass
---
+++ snake.py
@@ -0,0 +1,15 @@
+class snake(object):
+    def __init__(self, color, pos):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self, pos):
+        pass
+
+    def add_Cube(self):
+        pass
+
+    def draw(self, surface):
+        pass
